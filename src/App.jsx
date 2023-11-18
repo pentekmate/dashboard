58f9613c71365836128ab3cc62b006eea88a8fe9
pentekmate <90307397+pentekmate@users.noreply.github.com>
2023-11-18 12:33:08 +0100

    first push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='bg-red-700'>a</p>
+    </>
+  )
+}
+
+export default App
